87648ff17fc6b8042382f4ffd7ddd135d53d82b2
이초이 <choi1223@hunet.co.kr>
2024-04-25 11:18:15 +0900

    commit

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+export default function Home() {
+	return (
+		<div className="flex flex-col items-center justify-between min-h-screen p-6">
+			<div className="flex flex-col items-center justify-center gap-2 my-auto *:font-medium">
+				<span className="text-9xl">🥕</span>
+				<h1 className="text-4xl">당근</h1>
+				<h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
+			</div>
+			<div className="flex flex-col items-center w-full gap-3">
+				<Link
+					href="/create-account"
+					className="w-full text-lg text-white bg-orange-500 font-medium py-2.5 rounded-md text-center hover:bg-orange-400 transition-colors"
+				>
+					시작하기
+				</Link>
+				<div className="flex gap-2">
+					<span>이미 계정이 있나요?</span>
+					<Link href="/login" className="hover:underline">
+						로그인
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+}
